77d62b9707d59eefea4adb35fc4bedfc066fe4fe
Pinnoche <ptime552@gmail.com>
2025-01-11 23:45:01 +0100

    details page added with link parameter

--- articles/urls.py
+++ articles/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name='list'),
+    path('<slug:slug>/', views.article_detail, name='detail'),
     path('create', views.create),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 from .models import Article
 
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/article_list.html", {'articles': articles })
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, "articles/article_detail.html", {'article': article})
+    
 
 def create(request):
     return render(request, "articles/create.html")
