cf309b6e939525389b133cb8611f1a781cedca57
Pinnoche <ptime552@gmail.com>
2025-01-12 12:20:29 +0100

    auth login, signup, logout and require_login decorator

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Article
 
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, "articles/article_list.html", {'articles': articles })
+
+@login_required(login_url="{% url 'accounts:login' %}")
+def article_create(request):
+    return render(request, "articles/article_create.html")
 
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
